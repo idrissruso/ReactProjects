8431e52d231f3c37074cb4618e7e359c26473a42
Idrissa <idrisabkar2018@gmail.com>
2023-10-31 23:46:14 +0300

    bankist

--- card/src/App.js
+++ card/src/App.js
@@ -1,90 +1,121 @@
-import './App.css'
-import { useState } from 'react'
+/*
+INSTRUCTIONS / CONSIDERATIONS:
+
+1. Let's implement a simple bank account! It's similar to the example
+ that I used as an analogy to explain how useReducer works, but it's simplified (we're not using account numbers here)
+
+2. Use a reducer to model the following state transitions: openAccount, deposit, withdraw, requestLoan, payLoan, closeAccount. Use the `initialState` below to get started.
+
+3. All operations (expect for opening account) can only be performed if isActive is true. If it's not, just return the original state object. You can check this right at the beginning of the reducer
+
+4. When the account is opened, isActive is set to true. There is also a minimum deposit amount of 500 to open an account (which means that the balance will start at 500)
+
+5. Customer can only request a loan if there is no loan yet. If that condition is met, the requested amount will be registered in the 'loan' state, and it will be added to the balance. If the condition is not met, just return the current state
+
+6. When the customer pays the loan, the opposite happens: the money is taken from the balance, and the 'loan' will get back to 0. This can lead to negative balances, but that's no problem, because the customer can't close their account now (see next point)
+
+7. Customer can only close an account if there is no loan, AND if the balance is zero. If this condition is not met, just return the state. If the condition is met, the account is deactivated and all money is withdrawn. The account basically gets back to the initial state
+*/
+
+import { useReducer } from 'react'
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  isActive: false,
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'start':
+      return { ...state, isActive: true, balance: 500 }
+    case 'deposit':
+      return { ...state, balance: state.balance + 150 }
+    case 'withdraw':
+      return { ...state, balance: state.balance - 50 }
+    case 'loan':
+      return { ...state, balance: state.balance + 5000, loan: 5000 }
+    case 'payLoan':
+      return { ...state, balance: state.balance - state.loan, loan: 0 }
+    case 'clear':
+      return { ...initialState }
+    default:
+      throw new Error('Unknown action')
+  }
+}
 
 export default function App() {
-  const [bill, setBill] = useState(0)
-  const [service, setService] = useState(1)
-  const [friend, setFriend] = useState()
-
-  function handleBill(e) {
-    setBill(e.target.value)
-  }
-
-  function handleService(e) {
-    setService(e.target.value)
-  }
-
-  function handleFriend(e) {
-    setFriend(e.target.value)
-  }
-
-  let tip = bill * (service / 100) + bill * (friend / 100)
-
-  return (
-    <div>
-      <Bill bill={bill} onBillChange={handleBill} />
-      <MFeedback service={service} onServiceChange={handleService} />
-      <FFeedBack friend={friend} onFriendChange={handleFriend} />
-      <Result bill={bill} tip={tip} />
-      <button
-        onClick={() => {
-          setBill(0)
-          setService(0)
-          setFriend(0)
-        }}
-      >
-        Reset
-      </button>
-    </div>
+  const [{ balance, loan, isActive }, dispatch] = useReducer(
+    reducer,
+    initialState
   )
-}
 
-function Bill({ bill, onBillChange }) {
   return (
-    <div>
-      <label>How Much was the bill?</label>
-      <input type="text" onChange={(e) => onBillChange(e)} value={bill} />
-    </div>
-  )
-}
+    <div className="App">
+      <h1>useReducer Bank Account</h1>
+      <p>Balance: {balance}</p>
+      <p>Loan: {loan}</p>
 
-function MFeedback({ service, onServiceChange }) {
-  return (
-    <div>
-      <label>How was the service?</label>
-      <select onChange={(e) => onServiceChange(e)} value={service}>
-        <option value="1">very bad (0%)</option>
-        <option value="2">it was ok (5%)</option>
-        <option value="3">it was good (10%)</option>
-      </select>
-    </div>
-  )
-}
-
-function FFeedBack({ friend, onFriendChange }) {
-  return (
-    <div>
-      <label>How did your friend like the service?</label>
-      <select
-        value={friend}
-        onChange={(e) => {
-          onFriendChange(e)
-        }}
-      >
-        <option value="1">very bad (0%)</option>
-        <option value="2">it was ok (5%)</option>
-        <option value="3">it was good (10%)</option>
-      </select>
-    </div>
-  )
-}
-
-function Result({ bill, tip }) {
-  return (
-    <div>
-      <p>You pay {`$${bill}.0`}</p>
-      <p>Tip amount: {`$${tip}`}</p>
-      <p>Total: {bill + tip}</p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: 'start' })
+          }}
+          disabled={isActive}
+        >
+          Open account
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: 'deposit' })
+          }}
+          disabled={!isActive}
+        >
+          Deposit 150
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: 'withdraw' })
+          }}
+          disabled={!isActive || balance <= 0}
+        >
+          Withdraw 50
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: 'loan' })
+          }}
+          disabled={!isActive || loan}
+        >
+          Request a loan of 5000
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: 'payLoan' })
+          }}
+          disabled={!isActive || loan <= 0 || balance < loan}
+        >
+          Pay loan
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: 'clear' })
+          }}
+          disabled={!isActive || !(loan === 0)}
+        >
+          Close account
+        </button>
+      </p>
     </div>
   )
 }
